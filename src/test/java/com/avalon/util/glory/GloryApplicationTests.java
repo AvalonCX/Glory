7ac28997e3fdcd0dde86c9357badd3536a6fb6a7
rohit <rohit.sharma06596@gmail.com>
2022-06-18 22:58:23 -0400

    Initialize

---
+++ src/test/java/com/avalon/util/glory/GloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalon.util.glory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
